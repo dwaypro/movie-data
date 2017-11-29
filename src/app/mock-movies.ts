d064e66381a0fc88e9b47b91dd7c5e05ac08479f
Daniel Way <dwaypro@Daniels-MacBook-Pro.local>
2017-11-29 10:10:22 -0800

    mock photos

--- src/app/mock-movies.ts
+++ src/app/mock-movies.ts
@@ -1,16 +1,16 @@
 import { Movie } from './movie';
 
 export const MOVIES: Movie[] = [
-  { id: 11, title: 'Mr. Nice' },
-  { id: 12, title: 'Narco' },
-  { id: 13, title: 'Bombasto' },
-  { id: 14, title: 'Celeritas' },
-  { id: 15, title: 'Magneta' },
-  { id: 16, title: 'RubberMan' },
+  { id: 11, title: 'Black Friday', photo: '/assets/Friday.png' },
+  { id: 12, title: 'James Bond', photo: '/assets/James.jpg' },
+  { id: 13, title: 'Pinocchio' photo: '/assets/pinocchio.jpg' },
+  { id: 14, title: 'Last Samurai' photo: '/assets/lastsamurai.jpg' },
+  { id: 15, title: 'Gladiator' photo: '/assets/gladiator.jpg' },
+  { id: 16, title: 'The Shining' photo: '/assets/shining.jpg'},
   { id: 17, title: 'Dynama' },
   { id: 18, title: 'Dr IQ' },
   { id: 19, title: 'Magma' },
   { id: 20, title: 'Tornado' }
   { id: 21, title: 'Mega Man' }
-  { id: 22, title: 'Cat Woman' }
+  { id: 22, title: 'Cat Woman'}
 ];
