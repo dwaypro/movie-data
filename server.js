dd32e2c85e29976f3414612a8485079945810dba
dwaypro <dangrantway@gmail.com>
2017-12-15 11:05:35 -0800

    deploy

--- server.js
+++ server.js
@@ -1,25 +1,19 @@
 const express = require('express');
 const path = require('path');
-const http = require('http');
-const bodyParser = require('body-parser');
-
-const api = require('./server/routes/api');
 
 const app = express();
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false}));
 
 app.use(express.static(__dirname + '/dist'));
 
 app.use('/api', api);
 
+app.listen(process.env.PORT || 8080);
 
 app.get('/*', function(req, res){
 	res.sendFile(path.join(__dirname +'/dist/index.html'));
 })
 
-app.listen(process.env.PORT || 8080);
+
 
 
 console.log('server listening');
